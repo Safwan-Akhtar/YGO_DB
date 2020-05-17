58f3cb74d398c4f37e4c8f223af4834acc02a736
Safwan-Akhtar <safwan.qa@hotmail.com>
2020-05-18 00:39:24 +0100

    added services for cards database and custom exceptions

---
+++ src/main/java/org/example/service/CardService.java
@@ -0,0 +1,48 @@
+package org.example.service;
+
+import org.example.CardDB.Card_DB;
+import org.example.exceptions.CardNotFoundException;
+import org.example.repo.CardRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private final CardRepo repo;
+
+    @Autowired
+    public CardService(CardRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<Card_DB> readCards(){
+        return repo.findAll();
+    }
+
+    public Card_DB createCard(Card_DB card){
+        return this.repo.save(card);
+    }
+
+    public Card_DB findCardById(Long id){
+        return this.repo.findById(id).orElseThrow(CardNotFoundException::new);
+    }
+
+    public Card_DB updateCard(Long id, Card_DB card){
+        Card_DB update = findCardById(id);
+        update.setCardName(card.getCardName());
+        update.setCardType(card.getCardType());
+        return this.repo.save(update);
+    }
+
+    public boolean deleteCard(Long id){
+        if(!this.repo.existsById(id)){
+            throw new CardNotFoundException();
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
+
+}
