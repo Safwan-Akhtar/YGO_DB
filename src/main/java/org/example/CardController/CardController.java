085d39f65ab5a9d497e03e38cc04885d188c876a
Safwan-Akhtar <safwan.qa@hotmail.com>
2020-05-18 01:30:10 +0100

    added controller for cards.

--- src/main/java/org/example/CardController/CardController.java
+++ src/main/java/org/example/CardController/CardController.java
@@ -1,7 +1,25 @@
 package org.example.CardController;
 
+import org.example.CardDB.Card_DB;
+import org.example.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CardController {
+
+    private final CardService service;
+
+    @Autowired
+    public CardController(CardService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAllCards")
+    public List<Card_DB> getAllCards(){
+        return this.service.readCards();
+    }
 }
