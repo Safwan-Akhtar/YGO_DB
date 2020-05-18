e9782689dbde707d63fa257767ff3468e4bf774b
Safwan-Akhtar <safwan.qa@hotmail.com>
2020-05-18 01:50:24 +0100

    added create command for Card Database controller.

--- src/main/java/org/example/CardController/CardController.java
+++ src/main/java/org/example/CardController/CardController.java
@@ -4,6 +4,8 @@ import org.example.CardDB.Card_DB;
 import org.example.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,10 @@ public class CardController {
     @GetMapping("/getAllCards")
     public List<Card_DB> getAllCards(){
         return this.service.readCards();
+    }
+
+    @PostMapping("/createCard")
+    public Card_DB createCard(@RequestBody Card_DB card){
+        return this.service.createCard(card);
     }
 }
